3e1c79bdce6e8efd94dd56754e6b871e3210ff5c
𝚨𝛂ѕͱ꧊𝐢͢к <teamaashik@gmail.com>
2026-01-23 23:04:37 +0530

    Update Youtube.py

--- AloneMusic/platforms/Youtube.py
+++ AloneMusic/platforms/Youtube.py
@@ -6,9 +6,9 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch
-from ShrutiMusic.utils.formatters import time_to_seconds
+from AloneMusic.utils.formatters import time_to_seconds
 import aiohttp
-from ShrutiMusic import LOGGER
+from AloneMusic import LOGGER
 from typing import Union
 
 YOUR_API_URL = None
@@ -16,7 +16,7 @@ FALLBACK_API_URL = "https://shrutibots.site"
 
 async def load_api_url():
     global YOUR_API_URL
-    logger = LOGGER("ShrutiMusic.platforms.Youtube.py")
+    logger = LOGGER("AloneMusic.platforms.Youtube.py")
     
     try:
         async with aiohttp.ClientSession() as session:
